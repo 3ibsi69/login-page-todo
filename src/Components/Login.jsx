6e287e597b1a97f6313171914e57bb716f9c7fc7
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-29 21:30:47 +0100

    login verfication

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,51 @@
-function Login(){
-    return(
-        <div className="container">
-            <h1>Login</h1>
-            <div className="inputs">
-                <input id="email" type="email" placeholder="Email" />
-                <input id="password" type="password" placeholder="Password" />
-                <button  id="btn">Login</button>
-            </div>
-            <hr />
-            <p>
-                You don't have an account? <a href="/signup">Sign Up</a>
-            </p>
-            </div>
-    )
+import axios from "axios";
+import { useState } from "react";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const getLogin = () => {
+    axios.get("http://localhost:3636/signup").then((res) => {
+    const data=res.data;
+    const finduser=data.find((e)=>e.email===email&&e.password===password);
+    if(finduser){
+        alert("Hello"+finduser.name);
+
+  }else{
+    alert("Please enter a valid email or password");
+  }
+    });
+    
+  };
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <div className="inputs">
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button id="btn" onClick={getLogin}>
+          Login
+        </button>
+      </div>
+      <hr />
+      <p>
+        You don't have an account? <a href="/signup">Sign Up</a>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
