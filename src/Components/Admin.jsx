3ef049e35ed5320285302e55a32d66f41c37cd43
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-13 10:44:22 +0100

    fixed functionnality/added styling

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../Styles/adminStyle.css";
 
 function AdminForm() {
   const navigate = useNavigate();
   const [allUsers, setAllUsers] = useState([]);
-  const [todos,setTodos]=useState([]);
-  const [AdminId,setAdminId]=useState(null);
+  const [todos, setTodos] = useState([]);
+  const [AdminId, setAdminId] = useState(null);
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -35,86 +36,108 @@ function AdminForm() {
     await axios
       .get("http://localhost:3636/admin/getalluser/" + adminId)
       .then(({ data }) => {
-        // const filteredUsers = data.alluser.filter(user=>user._id!==adminId);
-        setAllUsers(data.alluser);
-        setAdminId(adminId)
+        const filteredUsers = data.alluser.filter(user=>user._id!==adminId);
+        setAllUsers(filteredUsers);
+        setAdminId(adminId);
       })
       .catch((error) => {
         console.error("Error getting all users:", error);
       });
   };
 
+  const changeRole = (userId, newRole) => {
+    axios
+      .put("http://localhost:3636/admin/changeRole/" + AdminId, {
+        id: userId,
+        role: newRole,
+      })
+      .then(() => {
+        getAllUsers(AdminId);
+      })
+      .catch((error) => {
+        console.log("error changing role ");
+      });
+  };
+  const deleteUser = (userId) => {
+    axios
+      .delete("http://localhost:3636/admin/deleteUser/" + AdminId, {
+        data: { id: userId },
+      })
+      .then(() => {
+        getAllUsers(AdminId);
+      })
+      .catch((error) => {
+        console.log("error deleting user ");
+      });
+  };
 
-  const changeRole = (userId,newRole)=>{
-  axios
-  .put("http://localhost:3636/admin/changeRole/" + AdminId, {
-    id:userId,
-    role: newRole,
-  })
-  .then(()=>{
-    getAllUsers(AdminId);
-
-  })
-  .catch((error)=>{
-    console.log("error changing role ")
-  })
-}
-const deleteUser = (userId)=>{
-  axios
-  .delete("http://localhost:3636/admin/deleteUser/" + AdminId, {
-    data:{id:userId}
-    })
-    .then(()=>{
-      getAllUsers(AdminId);
-      
-      }
-      )
-      .catch((error)=>{
-        console.log("error deleting user ")
+  const seetodos = (userId) => {
+    // Check if todos are already being displayed for the user
+    if (todos.length > 0 && todos[0].userId === userId) {
+      // If so, clear the todos state
+      setTodos([]);
+    } else {
+      // Fetch todos for the user
+      axios
+        .get("http://localhost:3636/todo/" + userId)
+        .then(({ data }) => {
+          setTodos(data);
         })
-        }
-          
-
-
-
-
-  return(
+        .catch((error) => {
+          console.log("Error fetching todos:", error);
+        });
+    }
+  };
+  
+  return (
     <div className="Users-list">
+      <div className="disconnect">
+      <button className="btn" onClick={() => navigate("/todo")}>Go Back</button>
+      </div>
+      <h1>All Users :</h1>
+      <div className="List">
+        {allUsers.map((e, index) => {
+          const counter = index + 1;
+          return (
+            <div className="users" key={e._id}>
+              <h3>User {counter}</h3>
+              <h4>Id</h4>
+              <p>{e._id}</p>
+              <h4>email</h4>
 
-    <h1>All Users :</h1>
-     <div className="List">
-      {allUsers.map((e,index)=>{
-          const counter=index+1;
-        return(
-        <div className="users" key={e._id}>
-          <h3>User {counter}</h3>
-          <h4>Id</h4>
-          <p>{e._id}</p>
-          <h4>email</h4>
+              <p>{e.email}</p>
+              <h4>role</h4>
 
-          <p>{e.email}</p>
-          <h4>role</h4>
+              <p>{e.role}</p>
+              <div className="admin-action">
+                <button className="btn" onClick={() => changeRole(e._id, "user")}>
+                  change to user
+                </button>
+                <button  className="btn" onClick={() => changeRole(e._id, "admin")}>
+                  change to admin
+                </button>
+                <button className="btn" onClick={() => deleteUser(e._id)}>delete user</button>
+                <button className="btn" onClick={() => seetodos(e._id)}>see todos</button>
+              </div>
+              
+              {todos.length > 0 && e._id === todos[0].userId && (
+                <div>
+                  <h4>Todos:</h4>
+                  <ul>
+                    {todos.map((todo) => (
+                      <li key={todo.id}> {todo.title}</li>
+                      
+                    ))}
+                  </ul>
+                </div>
+              )}
 
-          <p>{e.role}</p>
-          <div className="admin-action">
-          <button onClick={()=>changeRole(e._id,"user")}>change to user</button>
-          <button onClick={()=>changeRole(e._id,"admin")}>change to admin</button>
-          <button onClick={()=>deleteUser(e._id)}>delete user</button>
-
-
-
-          </div>
-          
-          </div>
-    )
-      })}
-      </div> 
+            </div>
+          );
+        })}
+      </div>
     </div>
-
-  )
-
+  );
 }
-
-
 
 export default AdminForm;
